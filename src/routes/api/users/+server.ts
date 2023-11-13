20675ac9f139ca5f8b2f73d6d4de21bd5b238a6b
Celeste De Schamphelaere <celestedeschamphelaere@protonmail.com>
2023-11-13 08:46:31 +0100

    fixed making users admin, server logic was not present

---
+++ src/routes/api/users/+server.ts
@@ -0,0 +1,38 @@
+import { error, json } from "@sveltejs/kit";
+import { prisma } from "$lib/Prisma";
+import type { RequestHandler } from "./$types";
+
+export const PUT = (async ({ request }) => {
+    //we deviate from the normal architecture of just inserting our normal data since the admin route is a mix of 2 tables
+    //and we dont want to make every api clal on it include thousands of user data, since thats just a lot of data
+    //instead we work in mutations, which look like this {id: number, isAdmin: boolean}
+
+    const res: any = await request.json(); //todo type
+
+    try {
+        for (const mutation of res) {
+            console.log(mutation);
+            if (mutation.isAdmin === "False") {
+                await prisma.admin.deleteMany({
+                    //this is only delete many because otherwise I cant use the AND selector. Why? ask the prisma devs...
+                    where: {
+                        AND: [{ user_id: mutation.id }, { organization: mutation.organization }],
+                    },
+                });
+
+                //insert linked admin data
+            } else if (mutation.isAdmin === "True") {
+                await prisma.admin.create({
+                    data: {
+                        user_id: mutation.id,
+                        organization: mutation.organization,
+                    },
+                });
+            }
+        }
+        return json({ count: res.length });
+    } catch (err) {
+        console.log(err);
+        throw error(500);
+    }
+}) satisfies RequestHandler;
