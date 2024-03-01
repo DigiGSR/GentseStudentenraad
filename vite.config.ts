32c019baf8e542e9e09a8cd2ee051428a6f3c763
Celeste De Schamphelaere <celestedeschamphelaere@protonmail.com>
2024-03-01 15:04:07 +0100

    added restart plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import ViteRestart from "vite-plugin-restart";
 
 const config: UserConfig = {
-    plugins: [sveltekit(), basicSsl()],
+    plugins: [
+        sveltekit(),
+        basicSsl(),
+        ViteRestart({
+            restart: ["static/**/*"],
+        }),
+    ],
 };
 
 export default config;
