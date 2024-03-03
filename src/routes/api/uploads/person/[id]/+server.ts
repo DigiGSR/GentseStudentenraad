9687ee36b5c5e3b499e69dc689c9eca688a7a545
Celeste De Schamphelaere <celestedeschamphelaere@protonmail.com>
2024-03-03 12:42:34 +0100

    file serving bugfix

--- src/routes/api/uploads/person/[id]/+server.ts
+++ src/routes/api/uploads/person/[id]/+server.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 
 export async function GET({ params }) {
     const name = params.id ?? "";
-    const data = fs.promises.readFile(`${process.env.UPLOAD_DIR}/${name}`);
+    const data = fs.promises.readFile(`${process.env.UPLOAD_DIR}/person/${name}`);
     return new Response(await data, {
         headers: { "Cache-Control": "max-age=604800, stale-while-revalidate=86400" },
     });
