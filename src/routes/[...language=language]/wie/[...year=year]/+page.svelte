2b0a5a348ca796c43fbe844351385f62546aea8c
Raul Popa <raul.popa@ugent.be>
2024-07-09 15:40:27 +0200

    Add table of content to Who page

--- src/routes/[...language=language]/wie/[...year=year]/+page.svelte
+++ src/routes/[...language=language]/wie/[...year=year]/+page.svelte
@@ -3,8 +3,17 @@
     import { goto } from "$app/navigation";
     import PositionCard from "$lib/components/PositionCard.svelte";
     import type { PageData } from "./$types";
+    import { onMount } from "svelte";
 
     let selected = "2022";
+
+    const sections: { id: string; name: string }[] = [];
+
+    $: {
+        for (const group of data.groups) {
+            sections.push({ id: group.id.toString(), name: group.name });
+        }
+    }
 
     export let data: PageData;
 </script>
@@ -17,6 +26,17 @@
         style="object-position: 50% 30%"
     />
 {/if}
+
+<div class="hidden absolute top-[600px] left-10 md:flex flex-col max-w-fit space-y-2">
+    <p class="text-sm text-black text-opacity-60">Raden & Comissies</p>
+    <div class="flex flex-col space-y-2 pl-4">
+        {#each sections as section}
+            <a href="#{section.id}" class="text-black text-opacity-70 hover:text-opacity-100"
+                >{section.name}</a
+            >
+        {/each}
+    </div>
+</div>
 
 <div class="container flex text-lg -mt-32 translate-y-10 justify-start md:justify-end gap-4">
     <p>Bekijk historische data:</p>
@@ -42,7 +62,7 @@
 <div class="space-y-12 py-12">
     <!-- {#each data.groups.filter((e) => e.positions.length > 0) as group} NOTE: this filters out all of the groups without members. Is this the desired behaviour?-->
     {#each data.groups as group}
-        <div class="odd:bg-neutral-100">
+        <div class="odd:bg-neutral-100" id={group.id.toString()}>
             <div class="container grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div class="space-y-1 md:col-span-2">
                     <p class="font-bold text-3xl">{group.name}</p>
