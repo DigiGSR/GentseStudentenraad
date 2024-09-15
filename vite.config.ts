fe7849b1e119f2d085402c37c4e957fe09685749
Celeste De Schamphelaere <coolestrogen@protonmail.com>
2024-09-15 15:16:42 +0200

    new favicon just dropped

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const config: UserConfig = {
         sveltekit(),
         basicSsl(),
         ViteRestart({
-            restart: ["static/**/*"],
+            restart: ["/app/client/uploads/*"],
         }),
     ],
     server: {
