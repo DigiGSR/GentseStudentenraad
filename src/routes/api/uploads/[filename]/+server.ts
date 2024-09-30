e55392b60666234dee2b5158851818d1f94c4c1d
Celeste De Schamphelaere <coolestrogen@protonmail.com>
2024-09-30 11:47:10 +0200

    oop2

--- src/routes/api/uploads/[filename]/+server.ts
+++ src/routes/api/uploads/[filename]/+server.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 export async function GET({ params }) {
     const name = params.filename;
-    console.log("bingus", fs.readdirSync("./api/uploads"));
-    const data = await fs.promises.readFile(`./api/uploads/${name}`);
+    console.log("bingus", fs.readdirSync("./uploads/api"));
+    const data = await fs.promises.readFile(`./uploads/api/${name}`);
     return new Response(data);
 }
